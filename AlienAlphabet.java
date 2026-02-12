35e33a91c3f9386910281ccf852cd933b249c3fd
Hemanth Gowda <hemanthagalahalli@gmail.com>
2026-02-12 12:22:09 +0530

    Add files via upload

---
+++ AlienAlphabet.java
@@ -0,0 +1,7 @@
+public class AlienAlphabet {
+    public static void main(String[] args) {
+        // Here the NUmerical Basically Tells that which Elemsnt SHould Appear Before Which Element 
+        // For Example IN Our English Dictinr
+    }
+    
+}
